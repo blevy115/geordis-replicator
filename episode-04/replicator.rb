315754bcb074239e5095acccc9b4240fe2dabcf9
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-07 14:54:52 -0500

    completed episode 4

--- episode-04/replicator.rb
+++ episode-04/replicator.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Replicator
 
   # When the Enterprise calls Replicator.new, this method executes.
@@ -40,7 +42,7 @@ class Replicator
   # and then manually execute each method to ensure
   # it returns what's expect.
   def replicate(recipe)
-
+# binding.pry
     # Setup an instance variable for the recipe
     # so that other methods can see what the recipe is
     @recipe = recipe
@@ -64,8 +66,8 @@ class Replicator
     #   glass_inside_replicator
     # And then to see what's inside the glass, use:
     #   glass_inside_replicator.inside.contents
+    # binding.pry
     transport_ingredients_to_glass
-
     # This methods mixes the ingredients in the glass around.
     # It returns nil, even if successful, but if you look at:
     #   glass_inside_replicator.inside.contents
@@ -104,6 +106,7 @@ class Replicator
 
   # This transports ingredients into the glass.
   def transport_ingredients_to_glass
+    # binding.pry
 
     # Abort if there is no glass inside the replicator.
     return unless glass_inside_replicator
@@ -114,7 +117,8 @@ class Replicator
       @enterprise.transporter.energize(
         # Geordi is in a jokey mood and
         # reprograms the replicator
-        @enterprise.pantry.find_ingredient('banana'),
+        # @enterprise.pantry.find_ingredient('banana'),
+        @enterprise.pantry.find_ingredient(ingredient_name),
         @enterprise.pantry.shelf,
         glass_inside_replicator.inside
       )
@@ -123,7 +127,7 @@ class Replicator
 
   # This mixes the ingredients around inside the replicator.
   def mix
-
+# binding.pry
     # Abort if there is no glass inside the replicator.
     return unless glass_inside_replicator
 
